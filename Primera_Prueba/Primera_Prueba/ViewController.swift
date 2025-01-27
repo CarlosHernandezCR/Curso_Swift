49f7674ed8f66c4711d326491be3ac35d833f9e1
Carlos Raul Hernández Campos <chernandezcampos@educa.madrid.org>
2025-01-26 16:59:02 -0800

    Primera ejecucion

---
+++ Primera_Prueba/Primera_Prueba/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Primera_Prueba
+//
+//  Created by Carlos Hernandez on 26/1/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var boton1: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func botonPulsado(_ sender: Any) {
+        print("BIENVENIDO")
+    }
+    
+}
+
