661deb251056a271fc50072b72d0dd0460bac8fe
Carlos Raul Hernández Campos <chernandezcampos@educa.madrid.org>
2025-02-06 14:48:45 -0800

    Teoría texto

--- ChatPrototype/ChatPrototype/ContentView.swift
+++ ChatPrototype/ChatPrototype/ContentView.swift
@@ -9,14 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack{
+        VStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 25){
             //Image(systemName: "globe").imageScale(.large)
             Text("Knock, knock!")
                 .padding()
                 .background(Color.yellow)
             Text("Who's there?")
+                .padding()
+                .background(Color.red)
+            Text("Your dad")
+                .shadow(radius: 10)
+                .padding()
+                .background(Color.yellow)
+            Text("Oh dad! Welcome")
+                .padding()
+                .background(Color.red)
+                .shadow(radius: 10)
         }
         .padding()
+        
     }
 }
 
