ff9ee946e0cc2d045bb0ef2525a0012ed7e0b8b0
Carlos Raul Hernández Campos <chernandezcampos@educa.madrid.org>
2025-02-15 16:35:27 -0800

    Aplicacion del tiempo con 2 vistas

--- WeatherForecast/WeatherForecast/ContentView.swift
+++ WeatherForecast/WeatherForecast/ContentView.swift
@@ -9,32 +9,95 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        HStack{
-            DayForecast(day: "Mon", isRainy: false, high: 70, low: 50)
-            DayForecast(day: "Tue", isRainy: true, high: 60, low: 40)
+        var lista = true
+        VStack{
+            Text("Pulsa en el boton para verlo todo o por lista")
+            Button(action: {
+                lista.toggle()
+            }){
+                Text("Pulsa Aqui")
+                    .padding()
+                    .foregroundColor(.blue)
+                    .cornerRadius(10)
+            }
+            if(lista){
+                HStack{
+                    DayForecast(day: "Mon", climate: "sun", high: 33, low: 20)
+                    DayForecast(day: "Tue", climate: "sun and clouds", high: 25, low: 14)
+                    DayForecast(day: "Wed", climate: "clouds", high: 18, low: 11)
+                }
+                HStack{
+                    DayForecast(day: "Thu", climate: "rain", high: 14, low: 7)
+                    DayForecast(day: "Fri", climate: "storm", high: 10, low: 4)
+                    DayForecast(day: "Sat", climate: "snow", high: 5, low: -5)
+                }
+                HStack{
+                    DayForecast(day: "Sun", climate: "fog", high: 12, low: 4)
+                }
+            }
+            else{
+                let forecasts = [
+                    DayForecast(day: "Mon", climate: "sun", high: 33, low: 20),
+                    DayForecast(day: "Tue", climate: "sun and clouds", high: 25, low: 14),
+                    DayForecast(day: "Wed", climate: "clouds", high: 18, low: 11),
+                    DayForecast(day: "Thu", climate: "rain", high: 14, low: 7),
+                    DayForecast(day: "Fri", climate: "storm", high: 10, low: 4),
+                    DayForecast(day: "Sat", climate: "snow", high: 5, low: -5),
+                    DayForecast(day: "Sun", climate: "fog", high: 12, low: 4)
+                ]
+                ScrollView(.horizontal){
+                    HStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 10, content: {
+                        ForEach(forecasts, id: \.day){
+                            forecast in forecast
+                        }
+                    })
+                    .padding()
+                }
+            }
         }
+        
     }
 }
 
 struct DayForecast: View {
     let day: String
-    let isRainy:Bool
+    let climate:String
     let high: Int
     let low: Int
     
     var iconName:String {
-        if isRainy{
-            return "cloud.rain.fill"
-        } else {
+        switch climate {
+        case "sun":
             return "sun.max.fill"
+        case "sun and clouds":
+            return "cloud.sun.fill"
+        case "clouds":
+            return "cloud.fill"
+        case "rain":
+            return "cloud.rain.fill"
+        case "storm":
+            return "cloud.bolt.fill"
+        case "snow":
+            return "cloud.snow.fill"
+        case "fog":
+            return "cloud.fog.fill"
+        default:
+            return "questionmark"
         }
     }
     
-    var iconColor: Color{
-        if isRainy{
+    var colorTLow:Color{
+        if(low<10){
             return Color.blue
-        } else {
-            return Color.yellow
+        }else{
+            return Color.primary
+        }
+    }
+    var colorTHigh:Color{
+        if(high>=25){
+            return Color.red
+        }else{
+            return Color.primary
         }
     }
     var body: some View{
@@ -42,13 +105,13 @@ struct DayForecast: View {
             Text(day)
                 .font(Font.largeTitle)
             Image(systemName: iconName)
-                .foregroundColor(iconColor)
                 .padding(5)
             Text("High: \(high)º")
                 .fontWeight(.semibold)
+                .foregroundColor(colorTHigh)
             Text("Low: \(low)º")
                 .fontWeight(Font.Weight.medium)
-                .foregroundColor(Color.secondary)
+                .foregroundColor(colorTLow)
         }
         .padding()
     }
@@ -56,6 +119,8 @@ struct DayForecast: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+        }
     }
 }
